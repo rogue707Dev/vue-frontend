206f7dbed752b70dfece249c640931c0ca7ca4f3
rogue707dev <rogue901126@protonmail.com>
2019-10-23 22:58:38 +0800

    node 12 ok

--- tests/unit/utils/common.test.js
+++ tests/unit/utils/common.test.js
@@ -4,13 +4,13 @@ describe('[utils/common]', () => {
   })
   afterAll(() => {
   })
-  it('1.1 測試載入外部js loadScript', async() => {
+  it('1.1 測試載入外部js loadScript', async () => {
     const jsPath = `https://code.jquery.com/jquery-3.4.1.min.js`
     const scriptId = 'myscript'
     await loadScript(jsPath, scriptId, true)
     expect(document.getElementById(scriptId).src).toBe(jsPath)
   })
-  it('1.2 測試載入外部style loadStyle', async() => {
+  it('1.2 測試載入外部style loadStyle', async () => {
     const stylePath = `https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css`
     const styleId = 'mystyle'
     await loadStyle(stylePath, styleId, true)
